a2de33aa0ff09e6a40d669f24926a6d5e97b8306
ykeara <ykeara1@gmail.com>
2020-09-25 21:39:48 -0500

    changed enum location (#10)
    * added enums for cards
    
    * change structure of enum

---
+++ src/types.tsx
@@ -0,0 +1,16 @@
+
+export enum CardValue{
+	Clubs1 = 0,	Clubs2 = 1,	Clubs3 = 2,	Clubs4 = 3,
+	Clubs5 = 4,	Clubs6 = 5,	Clubs7 = 6,	Clubs8 = 7,
+	Clubs9 = 8,	Clubs10 = 9, Clubs11 = 10, Clubs12 = 11,
+	Clubs13 = 12, Spades1 = 13,	Spades2 = 14, Spades3 = 15,
+	Spades4 = 16, Spades5 = 17,	Spades6 = 18, Spades7 = 19,
+	Spades8 = 20, Spades9 = 21,	Spades10 = 22, Spades11 = 23,
+	Spades12 = 24, Spades13 = 25, Diamonds1 = 26, Diamonds2 = 27,
+	Diamonds3 = 28,	Diamonds4 = 29,	Diamonds5 = 30,	Diamonds6 = 31,
+	Diamonds7 = 32,	Diamonds8 = 33,	Diamonds9 = 34,	Diamonds10 = 35,
+	Diamonds11 = 36, Diamonds12 = 37, Diamonds13 = 38, Hearts1 = 39,
+	Hearts2 = 40, Hearts3 = 41,	Hearts4 = 42, Hearts5 = 43,
+	Hearts6 = 44, Hearts7 = 45, Hearts8 = 46, Hearts9 = 47,
+	Hearts10 = 48, Hearts11 = 49, Hearts12 = 50, Hearts13 = 51
+	}

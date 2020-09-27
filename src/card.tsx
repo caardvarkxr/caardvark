bac85e499360c361ef6920619b3f8842cc023ffb
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2020-09-26 23:59:09 -0700

    [WIP] networking experiments

--- src/card.tsx
+++ src/card.tsx
@@ -1,22 +1,33 @@
-import { AvComposedEntity,  AvModel,  AvPrimitive, AvStandardGrabbable, AvTransform, GadgetSeedContainerComponent, MoveableComponent, PrimitiveType } from '@aardvarkxr/aardvark-react';
-import { AvVolume, EVolumeType, g_builtinModelBox, g_builtinModelCylinder, g_builtinModelPanel,  } from '@aardvarkxr/aardvark-shared';
+import { AvComposedEntity,  AvGadget,  AvGadgetList,  AvModel,  AvPrimitive, AvStandardGrabbable, AvTransform, GadgetSeedContainerComponent, MoveableComponent, NetworkedGadgetComponent, PrimitiveType, RemoteGadgetComponent } from '@aardvarkxr/aardvark-react';
+import { AvVolume, EVolumeType, g_builtinModelBox, g_builtinModelCylinder, g_builtinModelPanel, infiniteVolume, InitialInterfaceLock,  } from '@aardvarkxr/aardvark-shared';
+import bind from 'bind-decorator';
 import * as React from 'react';
 import {CardValue} from './types';
+const equal = require( 'fast-deep-equal' );
 
 type CardProps = {
-    card: CardValue;
+	card: CardValue;
+	moveCallback: Function;
 }
+
+type CardState = {
+}
+
+const k_PlayingCardInterface = "PlayingCardIf@1";
 
 const cardWidth = 0.057;
 const cardHeight = 0.08;
 const cardDepth = 0.001;
 const cardGrabMargin = 0.01;
 
-export const PlayingCard: React.FC<CardProps> = (props) => {
+export class PlayingCard extends React.Component<CardProps, CardState>{
 
-    const [ moveable, setMoveable ] = React.useState( new MoveableComponent( () => {}, false, true ) );
+	private moveableComponent: MoveableComponent;
+	private networkedComponent: NetworkedGadgetComponent;
+	private remoteComponent: RemoteGadgetComponent;
+	private gadgetListRef: React.RefObject<AvGadgetList> = React.createRef<AvGadgetList>();
 
-    const k_cardVolume =
+    private k_cardHitbox =
 	{
 		type: EVolumeType.AABB,
 		aabb: 
@@ -25,16 +36,74 @@ export const PlayingCard: React.FC<CardProps> = (props) => {
 			zMin: -cardDepth - cardGrabMargin, zMax: cardDepth + cardGrabMargin,
 			yMin: -cardHeight - cardGrabMargin, yMax: cardHeight + cardGrabMargin,	
 		}
-    } as AvVolume;
+	} as AvVolume;
+
+	constructor( props: any )
+	{
+
+		super( props );
+
+   		let remoteInitLocks: InitialInterfaceLock[] = [];
+
+		if( !AvGadget.instance().isRemote )
+		{
+			remoteInitLocks.push( {
+				iface: k_PlayingCardInterface,
+				receiver: null,
+				params: this.state
+			} );
+		}
+
+		let remoteLock = AvGadget.instance().initialInterfaces.find( ( value ) => 
+			value.iface == RemoteGadgetComponent.interfaceName );
+		this.moveableComponent =  new MoveableComponent( () => {}, false, true );
+		if( remoteLock )
+		{
+			this.remoteComponent = new RemoteGadgetComponent(this.onRemoteEvent);
+		}else{
+			// this gadget is master, or will be if the user enters a room
+			this.networkedComponent = new NetworkedGadgetComponent( remoteInitLocks, this.onRemoteEvent );
+		}
+
+	}
+
+	@bind
+	public onRemoteEvent(){
+
+	}
+
+	componentDidUpdate( prevProps: CardProps )
+	{
+	}
+	
+	public onTransformUpdated() {			
+	}
+
+	/** Sends an event to another instance of this gadget. If this gadget is
+	 * the master, the event is sent to all remote instances. If this gadget 
+	 * is remote, the event is sent to the master. If this gadget is not networked,
+	 * or if it is networked, but the user is not currently in a room, the 
+	 * event is discarded.
+	 */
+	public sendRemoteEvent( event: object, reliable: boolean )
+	{
+		this.networkedComponent?.sendEventToAllRemotes( event, reliable );
+		this.remoteComponent?.sendEventToMaster( event, reliable );
+	}
+
+	public render(){
 
 		return (
-			<AvComposedEntity components={[moveable]} volume={k_cardVolume}> 
+			<AvComposedEntity components={[this.moveableComponent]} volume={this.k_cardHitbox}> 
+				{ this.networkedComponent
+				&& <AvComposedEntity components={ [ this.networkedComponent]} volume={infiniteVolume()} /> }
 				<AvTransform scaleX={0.056 * 1.4} scaleY={0.001} scaleZ={0.0889 * 1.4} rotateX={90}> 
-					<AvModel uri={g_builtinModelPanel} useTextureFromUrl={"card_textures/" + CardValue[props.card] + ".png"} />
+					<AvModel uri={g_builtinModelPanel} useTextureFromUrl={"card_textures/" + CardValue[this.props.card] + ".png"} />
 				</AvTransform>
 				<AvTransform translateZ={-0.001} scaleX={0.056 * 1.4} scaleY={0.001} scaleZ={0.0889 * 1.4} rotateX={90}> 
 					<AvModel uri={g_builtinModelPanel} useTextureFromUrl={"card_textures/cardback.png"} />
 				</AvTransform>
 			</AvComposedEntity>
 		);
+	}
 }
--- src/deck.tsx
+++ src/deck.tsx
@@ -1,8 +1,12 @@
-import {  AvGrabButton, AvStandardGrabbable, AvTransform  } from '@aardvarkxr/aardvark-react';
-import { g_builtinModelBox, g_builtinModelGear, g_builtinModelHook, g_builtinModelPlus } from '@aardvarkxr/aardvark-shared';
+import {  AvGadget, AvGrabButton, AvStandardGrabbable, AvTransform  } from '@aardvarkxr/aardvark-react';
+import { EAction, EHand, g_builtinModelBox, g_builtinModelGear, g_builtinModelHook, g_builtinModelPlus, InitialInterfaceLock, MinimalPose, minimalPoseFromTransform } from '@aardvarkxr/aardvark-shared';
+import bind from 'bind-decorator';
 import React, {useState} from 'react';
 import { PlayingCard } from './card';
 import { CardValue} from './types';
+
+
+const k_DeckInterface = "CardDeckState@1";
 
 type DeckProps = {
 }
@@ -10,11 +14,17 @@ type DeckProps = {
 type DeckState = {
     drawnCards: CardValue[];
     remainingCards: CardValue[];
+    cardTransforms: MinimalPose[];
 }
 
+interface DeckNetworkEvent  {
+    type: "draw" | "reset" | "gather"
+    drawnCards?: CardValue[];
+    remainingCards?: CardValue[];
+}
 
 class CardDeck extends React.Component<DeckProps, DeckState>{
-
+    private m_grabbableRef = React.createRef<AvStandardGrabbable>();
 
 	constructor( props: any )
 	{
@@ -22,32 +32,97 @@ class CardDeck extends React.Component<DeckProps, DeckState>{
         super( props );
 
         let fullDeck = []
-        for (let i = 0; i < 52; i++) {
+        let transforms = []
+        for (let i = 0; i < 13; i++) {
             fullDeck.push(i);
+            transforms.push(new Array(7).fill(0));
         }                        
         this.shuffle(fullDeck);
 
         this.state = {
             drawnCards: [],
-            remainingCards: fullDeck
+            remainingCards: fullDeck,
+            cardTransforms: transforms
         }
 
     }
 
-    public gather(){
-        let len = this.state.drawnCards.length
-        let deck = this.state.remainingCards;
-        let drawn = this.state.drawnCards;
+    public componentDidMount(){
+        if( !AvGadget.instance().isRemote )
+		{
+			//AvGadget.instance().registerForSettings( this.onSettingsReceived );
+		}
+		else // We are remote!
+		{
+			let params = AvGadget.instance().findInitialInterface( k_DeckInterface )?.params as DeckState;
+			this.onStateReceived( params );			
+		}
+    }
 
-        for(let i = 0; i < len; i++){
-            deck.push(drawn.pop());
+    public componentDidUpdate()
+    {
+        if( !AvGadget.instance().isRemote )
+		{
+            let e: DeckNetworkEvent = { type: "reset",
+                                       drawnCards: this.state.drawnCards,
+                                       remainingCards: this.state.remainingCards };
+			this.m_grabbableRef.current?.sendRemoteEvent( e, true );
+		}
+    }
+
+    @bind
+    public onStateReceived(state: DeckState){
+        if(state){
+            this.setState(state);
         }
-        this.shuffle(deck);
+    }
 
-        this.setState({
-            remainingCards: deck,
-            drawnCards: []
-        });
+    @bind
+    public onRemoteEvent(event: DeckNetworkEvent){
+        switch(event.type){
+            case "reset":
+                if(!AvGadget.instance().isRemote){
+                    console.log("Received unexpected reset event on non-remote");
+                }
+                else{
+                    this.setState({drawnCards: event.drawnCards, remainingCards: event.remainingCards, 
+                                   cardTransforms: this.state.cardTransforms}); // Don't update the card transforms here
+                }
+                break;
+            case "draw":
+                this.drawCard();
+                break;
+            case "gather":
+                this.gatherCards();
+                break;
+        }
+    }
+
+    @bind
+    public gatherCards(){
+
+        if( AvGadget.instance().isRemote )
+		{
+			let e: DeckNetworkEvent = { type: "gather" };
+			this.m_grabbableRef.current?.sendRemoteEvent( e, true );
+		}
+		else
+		{
+            let len = this.state.drawnCards.length
+            let deck = this.state.remainingCards;
+            let drawn = this.state.drawnCards;
+
+            for(let i = 0; i < len; i++){
+                deck.push(drawn.pop());
+            }
+            this.shuffle(deck);
+
+            this.setState({
+                remainingCards: deck,
+                drawnCards: []
+            });
+        }
+
     }
 
     public shuffle(cards: CardValue[]){
@@ -61,35 +136,65 @@ class CardDeck extends React.Component<DeckProps, DeckState>{
         return cards;
     }
     
+    @bind
     public drawCard(){
-        if(this.state.remainingCards.length == 0){
-            return;
+        if( AvGadget.instance().isRemote )
+		{
+			let e: DeckNetworkEvent = { type: "draw" };
+			this.m_grabbableRef.current?.sendRemoteEvent( e, true );
+		}
+		else{
+            if(this.state.remainingCards.length == 0){
+                return;
+            }
+            let deck = this.state.remainingCards;
+            let drawn = this.state.drawnCards;
+
+            let temp = deck.pop();
+            drawn.push(temp);
+
+            this.setState({
+                remainingCards: deck,
+                drawnCards: drawn
+            });
         }
-        let deck = this.state.remainingCards;
-        let drawn = this.state.drawnCards;
+    }
 
-        let temp = deck.pop();
-        drawn.push(temp);
-
-        this.setState({
-            remainingCards: deck,
-            drawnCards: drawn
-        });
+    @bind
+    public onCardMoved(card: CardValue, pose: MinimalPose){
+        console.log("onCardMooo");
     }
 
     public render(){
+
+   		let remoteInitLocks: InitialInterfaceLock[] = [];
+
+		if( !AvGadget.instance().isRemote )
+		{
+			remoteInitLocks.push( {
+				iface: k_DeckInterface,
+				receiver: null,
+				params: this.state
+			} );
+		}
+
         return(
-            <AvStandardGrabbable modelUri={ g_builtinModelBox } modelScale={ 0.04 } remoteInterfaceLocks={[]}>
+            <AvStandardGrabbable modelUri={ g_builtinModelBox } modelScale={ 0.04 } remoteGadgetCallback={this.onRemoteEvent}
+              useInitialParent={true} remoteInterfaceLocks={remoteInitLocks} ref={this.m_grabbableRef} >
+
                 <AvTransform translateY={ 0.05 } rotateX={45}>
-                    <AvGrabButton modelUri={ "models/card_draw_icon.glb" } onClick={ this.drawCard.bind(this) } />
+                    <AvGrabButton modelUri={ "models/card_draw_icon.glb" } onClick={ this.drawCard } />
                 </AvTransform>
                 <AvTransform translateY={ 0.2 } >
                     {this.state.drawnCards.map(cardVal => (
-                        <PlayingCard card={cardVal} key={cardVal} />
+                        <AvTransform transform={this.state.cardTransforms[cardVal]} key={cardVal}>
+                            <PlayingCard card={cardVal} key={cardVal} moveCallback={this.onCardMoved }
+                            />
+                        </AvTransform>
                     ))}
                 </AvTransform>
                 <AvTransform translateY={ 0.00 } translateX={0.06} rotateX={45} rotateY={45}>
-                    <AvGrabButton modelUri={ "models/card_return_icon.glb" } onClick={ this.gather.bind(this) } />
+                    <AvGrabButton modelUri={ "models/card_return_icon.glb" } onClick={ this.gatherCards } />
                 </AvTransform>
             </AvStandardGrabbable>
         );
